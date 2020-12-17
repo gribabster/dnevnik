b99a52dcafee0837f0b270cad241fc24af02db37
Дмитрий Черных <65289564+gribabster@users.noreply.github.com>
2020-12-17 14:43:08 +0500

    Add files via upload

---
+++ Edit_Window.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace dnevnik
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)//Добавление данных
+        {
+            
+            string fio = tbfio.Text;
+            string group = tbgroup.Text;
+            string data = tbdata.Text;
+            string dataz = tbdataz.Text;
+            string tema = tbtema.Text;
+            int osenka = 0;
+            if (tbosenka.Text != "")
+            { 
+               osenka = Convert.ToInt32(tbosenka.Text);
+            }
+            string ssql = $"INSERT INTO student_tabl  VALUES ( '{fio}','{group}','{data}')"; //Запрос вставить данные в таблицу Table_1 - имя таблици
+            string ssql2 = $"INSERT INTO tema_tabl2 VALUES ( '{dataz}','{tema}','{osenka}')";
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnev;Integrated Security=True"; //prak - имя базы данных(Поменять на свою)
+            SqlConnection conn = new SqlConnection(connectionString); // Подключение к БД
+            conn.Open();// Открытие Соединения
+
+            SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
+            int number = command.ExecuteNonQuery();
+            
+            SqlConnection conn2 = new SqlConnection(connectionString); // Подключение к БД
+            conn2.Open();// Открытие Соединения
+            SqlCommand command2 = new SqlCommand(ssql2, conn2);
+            int number2 = command2.ExecuteNonQuery();
+            
+            MessageBox.Show("Данные добавлены!");
+          
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string data = tbdataz.Text;
+            string tema = tbtema.Text;
+            string osenka = tbosenka.Text;
+            
+            string ssql = $"UPDATE tema_tabl2 SET tema = '{tema}', osenka = '{osenka}' WHERE data_zanyati = '{data}'"; //Запрос
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnev;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString); // Подключение к БД
+            conn.Open();// Открытие Соединения
+
+            SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
+            SqlDataReader reader = command.ExecuteReader(); // Выаолнение запроса вывод информации
+            reader.Close();
+            
+            MessageBox.Show("Данные изменены!");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Console;
+using static System.Convert;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace dnevnik
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Вывод данных
+            string table = "student_tabl"; //Имя таблицы
+            string table2 = "tema_tabl2";
+            string ssql = $"SELECT * FROM {table} "; //Запрос 
+            string ssql2 = $"SELECT * FROM {table2} ";
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnev;Integrated Security=True"; //prak - имя базы данных(Поменять на свою)
+            SqlConnection conn = new SqlConnection(connectionString); // Подключение к Базе данных
+            conn.Open();// Открытие Соединения
+            SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
+            
+            SqlDataReader reader = command.ExecuteReader(); // Выполнение запроса вывод информации
+            
+
+            SqlConnection conn2 = new SqlConnection(connectionString); // Подключение к Базе данных
+            conn2.Open();// Открытие Соединения
+            SqlCommand command2 = new SqlCommand(ssql2, conn2);// Объект вывода запросов
+
+            SqlDataReader reader2 = command2.ExecuteReader();
+
+            lfio.Items.Clear();
+            lgroup.Items.Clear();
+            ldata.Items.Clear();
+            ltema.Items.Clear();
+            losenka.Items.Clear();
+            while (reader.Read() && reader2.Read()) //В цикле вывести всю информацию из таблицы
+            {
+                
+                lfio.Items.Add(reader[0]);
+                lgroup.Items.Add(reader[1]);
+                ldata.Items.Add(reader2[1]);
+                ltema.Items.Add(reader2[2]);
+                losenka.Items.Add(reader2[3]);
+            }
+            
+        }
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window Edit_Window = new Window1();
+            Edit_Window.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)//Удаление
+        {
+            
+                string data = ldata.ToString();
+                string ssql = $"DELETE FROM tema_tabl2 WHERE data_zanyati = '{data}'"; //Запрос
+                string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dnev;Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString); // Подключение к Базе данных
+                conn.Open();// Открытие Соединения
+
+                SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
+                SqlDataReader reader = command.ExecuteReader(); // Выполнение запроса и вывод информации
+
+            
+                ldata.Items.Clear();
+                ltema.Items.Clear();
+                losenka.Items.Clear();
+                while (reader.Read()) //В цикле вывести всю информацию из таблицы
+                {
+                ldata.Items.Add(reader[1]);
+                ltema.Items.Add(reader[2]);
+                losenka.Items.Add(reader[3]);
+                }
+
+            reader.Close();
+                MessageBox.Show("Изменения применены!");
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)//Вызов для изменений
+        {
+            Window Edit_Window = new Window1();
+            Edit_Window.Show();
+        }
+    }
+
+}
